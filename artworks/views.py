e5daa9ad88f0e3bfd006b05522a0be95d6140467
AdamAtli <adamatlisandgreen@gmail.com>
2026-05-07 19:53:05 +0100

    updated artwork html and view to fetch real data from database

--- artworks/views.py
+++ artworks/views.py
@@ -1,30 +1,18 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from data import artworks, sellers
+from artworks.models import Artwork
+
 
 
 def index(request):
-    # TODO: Retrieve data from database
-    # TODO: Populate a template with data coming from database
-    for artwork in artworks:
-        seller = [x for x in sellers if x["id"] == artwork["seller_id"]][0]
-        
-        artwork["seller_name"] = seller["name"]
-        artwork["seller_logo"] = seller["logo"]
+    artworks = Artwork.objects.all()
 
     return render(request, "artwork/artworks.html", {
         "artworks": artworks
     })
 
 def get_art_by_id(request, id):
-    # TODO: Retrieve data from database
-    # TODO: Populate a template with data coming from database
-    artwork = [x for x in artworks if x["id"] == int(id)][0]
-
-    seller = [x for x in sellers if x["id"] == artwork["seller_id"]][0]
-    artwork["seller_name"] = seller["name"]
-    artwork["seller_logo"] = seller["logo"]
+    artwork = get_object_or_404(Artwork, pk=id)
 
     return render(request, "artwork/artwork_details.html", {
         "artwork": artwork
