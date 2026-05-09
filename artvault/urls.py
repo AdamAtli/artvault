709b3acf442a61e744abfc50222c21d7c55f43ee
AdamAtli <adamatlisandgreen@gmail.com>
2026-05-09 22:37:47 +0100

    users can now log in and change passwords using the log in button or going to users/login. added a password reset sent to email

--- artvault/urls.py
+++ artvault/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('user/', include('user.urls')),
+
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
