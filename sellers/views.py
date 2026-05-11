aff1167060f3a12bc04f3b0666b14cab62912eaf
AdamAtli <adamatlisandgreen@gmail.com>
2026-05-11 23:07:38 +0100

    oopsy in merge

--- sellers/views.py
+++ sellers/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from artworks.models import Artwork
 from artworks.views import get_filtered_artworks
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import SellerProfileForm
 
 from sellers.models import Seller
 def index(request):
@@ -17,3 +20,23 @@ def get_seller_by_id(request, id):
     context['seller'] = seller
 
     return render(request, "seller/seller_details.html", context)
+
+@login_required
+def edit_seller_profile(request):
+    seller = request.user.seller
+
+    if request.method == "POST":
+        form = SellerProfileForm(request.POST, request.FILES, instance=seller)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your profile was successfully updated.")
+            return redirect("seller-detail", id=seller.id)
+
+    else:
+        form = SellerProfileForm(instance=seller)
+
+    return render(request, "seller/edit_seller_profile.html", {
+        "form": form,
+        "seller": seller,
+    })
