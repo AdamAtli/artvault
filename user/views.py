514d0e506b6918e05ee0e035912c3a4d85409443
Baldvinn <baldvin.hrafn@gmail.com>
2026-05-12 00:08:44 +0000

    (Wrong branch) implementing user registration

--- user/views.py
+++ user/views.py
@@ -1,10 +1,23 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.shortcuts import redirect
+from sellers.models import Seller
+from buyers.models import Buyer
 
 def register(request):
+    form = UserCreationForm()
     if request.method == 'POST':
-        print(1)
-    else:
-        return render(request, 'user/register.html', {
-            'form': UserCreationForm()
-        })
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            is_seller = request.POST.get('is_seller') == 'on'
+            if is_seller:
+                Seller.objects.create(user=user)
+            else:
+                Buyer.objects.create(user=user)
+            return redirect('login')
+
+    return render(request, 'user/register.html', {
+        'form': form
+    })
